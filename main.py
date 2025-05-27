75ef4c1819be8c3e700c86b956ff438314ad1ea2
Hyunseok <mongboy1@naver.com>
2025-05-27 16:39:04 +0900

    계산기 메인 함수 수정

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from subtract import subtract
 from git_project.multiply import multiply
 from git_project.divide import divide
 def calculator():
-    a = float(input("첫 번째 숫자: "))
-    b = float(input("두 번째 숫자: "))
+    a = float(input("1st number: "))
+    b = float(input("2nd number: "))
     op = input("연산자 (+, -, *, /): ")
     operations = {
     '+': add,
@@ -19,6 +19,6 @@ def calculator():
         except Exception as e:
             print("오류:", e)
     else:
-        print("올바르지 않은 연산자입니다.")
+        print("Invalid operator.")
 if __name__ == "__main__":
     calculator()
